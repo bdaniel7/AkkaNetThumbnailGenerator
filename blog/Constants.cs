c02734ec1f5a22b5603469c17187d87e65a3eadb
Daniel Blendea <bdaniel7@gmail.com>
2020-09-02 19:39:27 +0300

    fixed paths

--- blog/Constants.cs
+++ blog/Constants.cs
@@ -2,8 +2,8 @@ namespace AkkaNetThumbnailGenerator
 {
     public static class Constants
     {
-        public static readonly string PostsDirectory = @"E:\projects\bdaniel7\maartenba.github.io\_posts";
-        public static readonly string BackgroundImage = @"E:\projects\bdaniel7\maartenba.github.io\twitter.png";
-        public static readonly string OutputDirectory = @"E:\projects\bdaniel7\maartenba.github.io\out";
+        public static readonly string PostsDirectory = @"E:\projects\bdaniel7\AkkaNetThumbnailGenerator\_posts";
+        public static readonly string BackgroundImage = @"E:\projects\bdaniel7\AkkaNetThumbnailGenerator\twitter.png";
+        public static readonly string OutputDirectory = @"E:\projects\bdaniel7\AkkaNetThumbnailGenerator\out";
     }
 }
